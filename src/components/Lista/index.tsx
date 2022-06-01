dd8f40da2b50115d98cb3a892c81ad25f7ab0db2
Ary Plateira <ary_plateira@outlook.com>
2022-05-31 21:09:47 -0300

    Disponibilizar componente em uma props padrão chamada children

--- src/components/Lista/index.tsx
+++ src/components/Lista/index.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Item from "./Item";
 import style from './Lista.module.scss';
 //import { isTemplateTail } from "typescript";
 
@@ -18,11 +19,11 @@ function Lista(){
             <h2>Estudo do dia</h2>
             <ul>
                 {tarefas.map((item, index) => (
-                    <li key={index} className={style.item}>
-                        <h3>{item.tarefa}</h3>
-                        <span>{item.tempo}</span>
-                    </li>
-                ))}     
+                    <Item
+                    key={index}
+                    {...item}
+                    />
+                ))}
             </ul>
         </aside>
     )
