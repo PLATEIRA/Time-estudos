20b7fe2327cf905f74839c604b6877293d04147b
Ary Plateira <ary_plateira@outlook.com>
2022-06-01 17:59:59 -0300

    Acrescentou o valor this.state.tempo

--- src/components/Formulario/index.tsx
+++ src/components/Formulario/index.tsx
@@ -4,6 +4,10 @@ import Botao from "../Botao";
 import style from  './Formulario.module.scss';
 
 class Formulario extends React.Component {
+    state = {
+        tarefa: "";
+        tempo: "00:00:00";
+    }
     render() {
         return(
             <form className={style.novaTarefa}>
@@ -27,6 +31,7 @@ class Formulario extends React.Component {
                         type="time"
                         step="1"
                         name="tempo"
+                        value={this.state.tempo}
                         id="tempo"
                         min="00:00:00"
                         max="01:30:00"
